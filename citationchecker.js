3b2daab74fdc7fc05011c0f43fc94c92490eb4ed
drumfiend21 <drumfiend21@gmail.com>
2015-01-20 22:24:51 -0500

    updated citationchecker.html and citationchecker.js

--- citationchecker.js
+++ citationchecker.js
@@ -4,40 +4,41 @@
 
 var text = "Citations: 'Solving emotion logic paradigms,' Siddharth Joshi, Journal of Psychology. 2013; 'Rattling the cage: questions of altruism in the meat industry', Michael Baliff, Ethics. 2014; 'The unified field: unified in relation to what?' Siddharth Joshi, Journal of Physics. 2015."
 
-console.log(text);
+var confirm = confirm("Do you want to search the article on the page for number of times your research was cited?");
 
-confirm("Do you want to search this article for number of times your research was cited?");
+if(confirm === true)
+  {
+  var yourName = "Siddharth"
 
-var yourName = "Siddharth"
+  var hits = []
 
-var hits = []
-
-for(var i=0; i< text.length; i++)
-  { 
-      if(text[i]==="S")
-        {
-        if(text[i+8]==="h")
-          {        
-          for(var j=i; j<(yourName.length+i); j++)
-            {
-            hits.push(text[j]);
+  for(var i=0; i< text.length; i++)
+    { 
+        if(text[i]==="S")
+          {
+          if(text[i+8]==="h")
+            {        
+            for(var j=i; j<(yourName.length+i); j++)
+              {
+              hits.push(text[j]);
+              }
             }
           }
-        }
-  }
-if(hits.length === 0)
-  {
-  console.log("Your name wasn't found!");
-  }
-  
-var count = 0;
-    
-  for(var i=0; i< hits.length; i++)
-    {
-    if(hits[i]==="S")
-      {
-      count+=1;
-      }
     }
+  if(hits.length === 0)
+    {
+    console.log("Your name wasn't found!");
+    }
+    
+  var count = 0;
+      
+    for(var i=0; i< hits.length; i++)
+      {
+      if(hits[i]==="S")
+        {
+        count+=1;
+        }
+      }
 
-console.log("You were cited in this research " + count + " times.");
+  console.log("You were cited in this research " + count + " times.");
+  }
